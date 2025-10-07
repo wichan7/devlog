030cb7c6d77d2b4fb6145f97ce1feee83136532d
wichan <wichan7@naver.com>
2025-10-07 17:45:37 +0900

    fix: sitemap 경로 /posts/posts 현상 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,13 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://blog.wichan.dev',
   generateRobotsTxt: true,
+  transform: async (_, path) => {
+    const newPath = path.replace(/\/posts\/posts\//, '/posts/');
+    return {
+      loc: `${process.env.SITE_URL || 'https://blog.wichan.dev'}${newPath}`,
+      changefreq: 'weekly',
+      priority: 0.7,
+    }
+  }
 }
+
