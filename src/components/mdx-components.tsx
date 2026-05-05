0a76ee681d5f671f796aa1dd261262421722a620
wichan <wichan7@naver.com>
2026-05-05 17:09:26 +0900

    fix: 내부링크 상대경로 처리

--- src/components/mdx-components.tsx
+++ src/components/mdx-components.tsx
@@ -1,8 +1,20 @@
 import Image from "next/image"
 import { useMDXComponent } from "next-contentlayer/hooks"
+import { Link } from "@/i18n/navigation"
+
+function MdxLink({
+  href,
+  ...props
+}: React.AnchorHTMLAttributes<HTMLAnchorElement>) {
+  if (!href || !href.startsWith("/")) {
+    return <a href={href} {...props} />
+  }
+  return <Link href={href as Parameters<typeof Link>[0]["href"]} {...props} />
+}
 
 const components = {
   Image,
+  a: MdxLink,
 }
 
 interface MdxProps {
